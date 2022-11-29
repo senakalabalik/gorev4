5ae5490267fd3104e3f568d9a67ba8b42f9515e6
sena kalabalık <46752539+senakalabalik@users.noreply.github.com>
2022-11-29 17:50:11 +0300

    Update gorev4.cpp

--- gorev4.cpp
+++ gorev4.cpp
@@ -1,1 +1,17 @@
+#include <iostream>
+#include <string>
+
+void Province (string a) ; // emin değilim bu kısımdan
+
+struct Country {
+  std::string name;
+  std::string capital; // int* ptr = &Province ;
+};
+struct Province {
+  std::string name;
+  std::string domain; // int*ptrr = &Country ;
+  
+ print .. ptrr*
+};
+
 
